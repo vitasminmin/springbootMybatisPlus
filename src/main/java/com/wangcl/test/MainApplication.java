a55d1ded78a24477467b09bac48a8c385441da51
vitasminmin <568802022@qq.com>
2019-03-09 09:30:51 +0800

    init

---
+++ src/main/java/com/wangcl/test/MainApplication.java
@@ -0,0 +1,18 @@
+package com.wangcl.test;
+
+//import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ *  @SpringBootApplication，标注在某个类上，说明这个类是springboot的主配置类，
+ *  springboot会运行这个类的main方法来启动springboot
+ * */
+
+//@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ src/main/java/com/wangcl/test/entity/CommonCode.java
@@ -0,0 +1,113 @@
+package com.wangcl.test.entity;
+
+
+public class CommonCode {
+    private String id;
+    private String typeCode;
+    private String paramId;
+    private String paramName;
+    private String createdBy;
+    private String dateCreated;
+    private String updatedBy;
+    private String dateUpdated;
+    private String series;
+    private String typeName;
+    private String parentName;
+    private String parentCode;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTypeCode() {
+        return typeCode;
+    }
+
+    public void setTypeCode(String typeCode) {
+        this.typeCode = typeCode;
+    }
+
+    public String getParamId() {
+        return paramId;
+    }
+
+    public void setParamId(String paramId) {
+        this.paramId = paramId;
+    }
+
+    public String getParamName() {
+        return paramName;
+    }
+
+    public void setParamName(String paramName) {
+        this.paramName = paramName;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public String getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(String dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
+    public String getParentCode() {
+        return parentCode;
+    }
+
+    public void setParentCode(String parentCode) {
+        this.parentCode = parentCode;
+    }
+}
